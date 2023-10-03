a32ffc2ab3b0bc7e54d2ec2a95261de28d540993
dandix25 <62560111+dandix25@users.noreply.github.com>
2023-10-03 15:57:36 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,5 +1,7 @@
 import data from 'https://chumley.barstoolsports.com/dev/data/games/eed38457-db28-4658-ae4f-4d4d38e9e212.json' assert { type: 'json' };
 console.log(data);
+import colors from 'https://unpkg.com/mlbcolors@0.0.2/mlbcolors/data.json' assert { type: 'json' };
+console.log(colors);
 
 /* Away Team Data */
 document.getElementById('away-abv').innerText = data.away_team.abbreviation;
