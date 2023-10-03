c2e202001a283cb42ff02c297667c673c387bf5c
dandix25 <62560111+dandix25@users.noreply.github.com>
2023-10-03 15:27:06 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,2 @@
-fetch('https://chumley.barstoolsports.com/dev/data/games/eed38457-db28-4658-ae4f-4d4d38e9e212.json')
-    .then((response) => response.json())
-    .then((json) => console.log(json));
+import data from 'https://chumley.barstoolsports.com/dev/data/games/eed38457-db28-4658-ae4f-4d4d38e9e212.json' assert { type: 'json' };
+console.log(data);
