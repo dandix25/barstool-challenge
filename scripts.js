ad7fdf218bf36cf81ff9537ddb0a1087e1b9cf12
dandix25 <62560111+dandix25@users.noreply.github.com>
2023-10-03 15:26:17 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,2 +1,3 @@
-import data from './scripts.json';
-console.log(data);
+fetch('https://chumley.barstoolsports.com/dev/data/games/eed38457-db28-4658-ae4f-4d4d38e9e212.json')
+    .then((response) => response.json())
+    .then((json) => console.log(json));
